eefbda20a684783e17a4b9c5eacec9237e8038e6
emiperez997 <emi.perez997@gmail.com>
2024-06-13 05:46:38 -0300

    [ADD] Clase 2

--- clase_2/proyecto-2/src/app/app.module.ts
+++ clase_2/proyecto-2/src/app/app.module.ts
@@ -7,6 +7,16 @@ import { MyHeaderComponent } from './components/my-header/my-header.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { StudentsComponent } from './components/students/students.component';
+import { NgIconsModule } from '@ng-icons/core';
+
+import {
+  heroUsersSolid,
+  heroAcademicCapSolid,
+  heroBriefcaseSolid,
+  heroHomeSolid,
+  heroMapSolid,
+  heroEnvelopeSolid,
+} from '@ng-icons/heroicons/solid';
 
 @NgModule({
   declarations: [
@@ -14,13 +24,21 @@ import { StudentsComponent } from './components/students/students.component';
     MyHeaderComponent,
     ToolbarComponent,
     SidebarComponent,
-    StudentsComponent
+    StudentsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgIconsModule.withIcons({
+      heroUsersSolid,
+      heroAcademicCapSolid,
+      heroBriefcaseSolid,
+      heroHomeSolid,
+      heroMapSolid,
+      heroEnvelopeSolid,
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- clase_2/proyecto-2/src/app/components/students/students.component.ts
+++ clase_2/proyecto-2/src/app/components/students/students.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-students',
   templateUrl: './students.component.html',
-  styleUrl: './students.component.scss'
+  styleUrl: './students.component.scss',
 })
 export class StudentsComponent {
-
+  students = [
+    { name: 'Juan', age: 20, email: 'juan@gmail.com' },
+    { name: 'Maria', age: 22, email: 'maria@gmail.com' },
+    { name: 'Pedro', age: 18, email: 'pedro@gmail.com' },
+    { name: 'Ana', age: 21, email: 'ana@gmail.com' },
+    { name: 'Luis', age: 19, email: 'luis@gmail.com' },
+  ];
 }
